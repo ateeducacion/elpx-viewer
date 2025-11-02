1392b0579b98d5bfa27a67758e299d6f49b60981
Ernesto Serrano <erseco@gmail.com>
2025-11-02 22:17:12 +0000

    Fourth version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
   githubClientId: 'Ov23lifSHlTbyzfO8lUx',
   defaultPagesBranch: 'gh-pages',
-  deviceFlowProxy: 'https://cors.isomorphic-git.org'
+  deviceFlowProxy: 'https://elpx-viewer.erseco.workers.dev/'
 };
