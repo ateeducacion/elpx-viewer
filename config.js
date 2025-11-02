1f7023c2ad41f85301a25e966de8567094b3356f
Ernesto Serrano <erseco@gmail.com>
2025-11-02 16:40:44 +0000

    Third version

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.APP_CONFIG = {
-  githubClientId: 'REPLACE_WITH_GITHUB_OAUTH_APP_CLIENT_ID',
+  githubClientId: 'Ov23lifSHlTbyzfO8lUx',
   defaultPagesBranch: 'gh-pages',
   deviceFlowProxy: 'https://cors.isomorphic-git.org'
 };
